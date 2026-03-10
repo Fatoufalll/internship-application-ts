d54593d240a99356a3e5a218d5b81e1220764ab6
Fatou Fall <ffall2163@gmail.com>
2026-03-10 09:20:07 +0100

    mise a jour du projet

--- lib/prisma.config.ts
+++ lib/prisma.config.ts
@@ -1,27 +1,21 @@
-import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not defined");
-}
+import "dotenv/config"
+import { PrismaClient } from "@prisma/client"
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined
 }
 
-export const prisma = 
-  global.prisma ?? 
+export const prisma =
+  global.prisma ??
   new PrismaClient({
     datasources: {
       db: {
-        url: databaseUrl as string, // Assertion de type pour garantir que c'est une string
+        url: process.env.DATABASE_URL!,
       },
     },
     log: process.env.NODE_ENV === "development" ? ["query", "error"] : [],
-  });
+  })
 
 if (process.env.NODE_ENV !== "production") {
-  global.prisma = prisma;
+  global.prisma = prisma
 }
